7f3177fa473b616a0d041759ed82b6a55f661312
kktheomniscient <kk.the.omniscient@gmail.com>
2025-10-03 12:12:51 +0530

    ip

--- frontend/src/app/player5/page.tsx
+++ frontend/src/app/player5/page.tsx
@@ -69,7 +69,7 @@ export default function Player1Page() {
 
   const connectWebSocket = () => {
     try {
-      wsRef.current = new WebSocket("ws://10.16.23.9:6789");
+      wsRef.current = new WebSocket("ws://192.168.2.190:6789");
       // wsRef.current = new WebSocket('ws://localhost:6789')
 
       wsRef.current.onopen = () => {
